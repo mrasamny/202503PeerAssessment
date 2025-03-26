b515e2ac99b9c2a22fd3c1df36d3273acad00ec2
mrasamny <mrasamny@desu.edu>
2025-03-26 15:54:03 -0400

    added scaleTitle

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import './rubric.css'
 import ScaleItem from './ScaleItem'
+import ScaleTitle from './ScaleTitle';
+
 
 function App() {
   function handleScaleChange(name, id, value){
@@ -9,11 +11,14 @@ function App() {
   }
 
   return (
-    <ScaleItem name="1|1" 
-      display="Jack Smith" 
-      scales={[{id: 1, value: 0},{id: 2, value: 1}, {id:3, value: 2}]}
-      handleScaleChange={handleScaleChange}
-    />
+    <>
+      <ScaleTitle scales={[{id: 1, value: 0},{id: 2, value: 1}, {id:3, value: 2}]}/>
+      <ScaleItem name="1|1" 
+        display="Jack Smith" 
+        scales={[{id: 1, value: 0},{id: 2, value: 1}, {id:3, value: 2}]}
+        handleScaleChange={handleScaleChange}
+      />
+    </>
   )
 }
 
--- front-end/src/ScaleTitle.jsx
+++ front-end/src/ScaleTitle.jsx
@@ -1,7 +1,16 @@
 import './rubric.css'
 
-export default ScaleTitle({scales}){
+export default function ScaleTitle({scales}){
     return (
-
+        <div className="row row-header">
+            <div className="blank border"></div>
+            <div className="scale-header">
+                {scales.map((item, index) =>
+                    <div key={index} className="scale-title border">
+                        {item.value}
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
