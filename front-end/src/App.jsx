8c42d7174a3fd3af04f76e18a76674a920a87eb5
mrasamny <mrasamny@desu.edu>
2025-03-31 19:13:23 -0400

    added comments for required work to be submitted on Wednesday

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,9 +11,11 @@ const baseURL = 'http://127.0.0.1:12000'
 
 function App() {
   const [rubric, setRubric] = useState(null)
+  const [group, setGroup] = useState(null)
 
   useEffect(()=>{
-    async function loadData(){
+
+    async function loadRubricData(){
       const response = await axios.get(`${baseURL}/assessment/1`)
       //console.log(response.data)
       let rubricData = {}
@@ -41,7 +43,30 @@ function App() {
      })
      setRubric(rubric)
     }
-    loadData()
+
+    /**
+     * Fetches group data (i.e. students in a group) with group_id of 1
+     * from the backend and places the data in the state variable group, 
+     * which has already been allocated using useState.  The
+     * variable group is an array of objects. Each object contains 
+     * groud_id, id (the primary key), first_name, last_name, email.
+     * For example, for the group with group_id of 1, 
+     * [{"group_id":1,"id":1,"email":"ayang@students.desu.edu","first_name":"Alena","last_name":"Yang"},
+     * {"group_id":1,"id":2,"email":"rstephenson@students.desu.edu","first_name":"Ross","last_name":"Stephenson"},
+     * {"group_id":1,"id":3,"email":"snash@students.desu.edu","first_name":"Susan","last_name":"Nash"},
+     * {"group_id":1,"id":4,"email":"pphelps@students.desu.edu","first_name":"Phoenix","last_name":"Phelps"},
+     * {"group_id":1,"id":5,"email":"dgonzalez@students.desu.edu","first_name":"Deanna","last_name":"Gonzalez"}]
+     * 
+     * use the endpoint `${baseURL}/groupMembers/1 to fetch the students in 
+     * group with group_id=1.
+     */
+    async function loadGroupData(){
+      // TODO: replace this line with your code
+    }
+
+    loadGroupData()
+    loadRubricData()
+
   },[])
 
   function handleScaleChange(name, id, value){
@@ -51,13 +76,16 @@ function App() {
   return (
       !rubric ? <p>Loading data...</p>:
         <>
-        <CatalogTitle name="Timliness" description="Blah blah blah"/>
+        {
+        // TODO: replace this line with your code to display peer assessment sheet
+
+        /* <CatalogTitle name="Timliness" description="Blah blah blah"/>
         <ScaleTitle scales={[{id: 1, value: 0},{id: 2, value: 1}, {id:3, value: 2}]}/>
         <ScaleItem name="1|1" 
           display="Jack Smith" 
           scales={[{id: 1, value: 0},{id: 2, value: 1}, {id:3, value: 2}]}
           handleScaleChange={handleScaleChange}
-        />
+        /> */}
         </>
   )
 }
